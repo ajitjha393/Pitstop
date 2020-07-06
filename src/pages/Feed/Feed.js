01271d0e495e2e397b45ddbfef4c44e8570df0fc
Bishwajit Jha <ajitjha393@gmail.com>
2020-07-06 19:19:55 +0530

    Fixed error of rendering 2  posts

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -198,11 +198,11 @@ class Feed extends Component {
 							p => p._id === prevState.editPost._id
 						);
 						updatedPosts[postIndex] = post;
-					} /*else if (
+					} /*if (prevState.posts.length < 2)*/ /*else if (
 						prevState.posts.length < 2
 					) {
 						updatedPosts = prevState.posts.concat(post);
-					}*/ /*if (prevState.posts.length < 2)*/
+					}*/
 					return {
 						posts: updatedPosts,
 						isEditing: false,
