1814a085502fb6937914649e2f19876a0d08df92
Bishwajit Jha <ajitjha393@gmail.com>
2020-07-18 19:30:58 +0530

    Stored New Post in DB and State

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -209,8 +209,17 @@ class Feed extends Component {
 					createdAt: resData.data.createPost.createdAt,
 				};
 				this.setState(prevState => {
+					let updatedPosts = [...prevState.posts];
+					if (prevState.editPost) {
+						const postIndex = prevState.posts.findIndex(
+							p => p._id === prevState.editPost._id
+						);
+						updatedPosts[postIndex] = post;
+					} else {
+						updatedPosts.unshift(post);
+					}
 					return {
-						post,
+						posts: updatedPosts,
 						isEditing: false,
 						editPost: null,
 						editLoading: false,
