c74f27419a416e5cb41ccd81993fbb89d9152858
Bishwajit Jha <ajitjha393@gmail.com>
2020-07-20 18:12:19 +0530

    Fixed Pagination Bug after adding new post

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -276,13 +276,17 @@ class Feed extends Component {
 				};
 				this.setState(prevState => {
 					let updatedPosts = [...prevState.posts];
+					let updatedTotalPosts = prevState.totalPosts;
 					if (prevState.editPost) {
 						const postIndex = prevState.posts.findIndex(
 							p => p._id === prevState.editPost._id
 						);
 						updatedPosts[postIndex] = post;
 					} else {
-						updatedPosts.pop();
+						updatedTotalPosts++;
+						if (prevState.posts.length >= 2) {
+							updatedPosts.pop();
+						}
 						updatedPosts.unshift(post);
 					}
 					return {
@@ -290,6 +294,7 @@ class Feed extends Component {
 						isEditing: false,
 						editPost: null,
 						editLoading: false,
+						totalPosts: updatedTotalPosts,
 					};
 				});
 			})
